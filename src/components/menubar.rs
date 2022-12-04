1383152f66a97dfd57a133c901e91466e32a3ec6
Shrey Somaiya <shrey.somaiya@gmail.com>
2022-12-04 18:38:56 +0100

    add file opening

--- src/components/menubar.rs
+++ src/components/menubar.rs
@@ -2,10 +2,7 @@ use crate::components::secondary_panel::{PanelType, SecondaryPanel};
 use crate::editor::files::{FileList, FileListAction};
 use crate::{toggle_secondary_pane, SplitContainer};
 use bounce::{use_atom, use_slice};
-use gloo::file::File;
 use stylist::yew::styled_component;
-use wasm_bindgen_futures::spawn_local;
-use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 #[styled_component(MenuBar)]
@@ -13,43 +10,6 @@ pub fn menubar() -> Html {
     let files = use_slice::<FileList>();
     let show_secondary_panel = use_state_eq(|| false);
     let panel_type = use_state_eq(|| PanelType::FileExplorer);
-
-    let _onchange = {
-        Callback::from(move |e: Event| {
-            let input: HtmlInputElement = e.target_unchecked_into();
-            let files = files.clone();
-            // for every file, read it and
-            // add it to the FileList
-            let mut result = Vec::new();
-
-            if let Some(files) = input.files() {
-                let files = js_sys::try_iter(&files)
-                    .unwrap()
-                    .unwrap()
-                    .map(|v| web_sys::File::from(v.unwrap()))
-                    .map(File::from);
-                result.extend(files);
-            }
-
-            for file in result.into_iter() {
-                let name = file.name();
-                let contents = gloo::file::futures::read_as_bytes(&file);
-                let files = files.clone();
-                spawn_local(async move {
-                    let contents = contents.await;
-
-                    // TODO(error handling): handle invalid utf8
-                    let contents = contents.expect("File contains invalid utf8");
-
-                    let contents = String::from_utf8(contents).unwrap();
-
-                    files.dispatch(FileListAction::Append(name, contents));
-
-                    files.dispatch(FileListAction::Log);
-                });
-            }
-        })
-    };
 
     // onclick to toggle the secondary panel
     let onclick = |panel_val: PanelType| {

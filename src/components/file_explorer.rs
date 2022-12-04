c2ae7f47ee7ccee948f74d1395d3eca9d1c26c5e
Shrey Somaiya <shrey.somaiya@gmail.com>
2022-12-04 15:13:24 +0100

    file name in explorer (with selected bg) + new file stub button

--- src/components/file_explorer.rs
+++ src/components/file_explorer.rs
@@ -1,42 +1,67 @@
-use crate::editor::files::{FileInfo, FileList};
+use crate::components::new_file_button::NewFileButton;
+use crate::editor::files::FileList;
 use bounce::use_slice;
 use stylist::yew::styled_component;
 use yew::prelude::*;
 
-impl From<FileInfo> for Html {
-    fn from(file_info: FileInfo) -> Self {
-        html! {
-            <div>
-                {file_info.name}
-            </div>
-        }
-    }
-}
-
 #[styled_component(FileExplorer)]
 pub fn file_explorer() -> Html {
     let files = &use_slice::<FileList>().files;
-
+    let selected = &use_slice::<FileList>().selected;
     html! {
         <>
             <div class={css!(r#"
                 display: flex;
                 flex-direction: row;
-                justify-content: flex-end;
+                justify-content: space-between;
+                margin-bottom: 0.5rem;
             "#)}>
+                <div class={css!(r#"
+                    font-weight: bold;
+                "#)}>
+                    { "Files" }
+                </div>
                 // add three checkboxes
-                <input type="button" title="test button" />
-                <input type="button" title="test buton2" />
-                <input type="button" title="test button3" />
+                <div>
+                    <NewFileButton />
+                </div>
             </div>
             {
-                for files.iter().map(|file_info| {
+                for files.iter().enumerate().map(|(index, file_info)| {
+                    let selected = selected.is_some() && selected.unwrap() == index;
                     html! {
-                        {file_info.clone()}
+                        <FileName name={file_info.name.clone()} {selected}/>
                     }
                 })
             }
         </>
 
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Properties)]
+pub struct FileNameProps {
+    pub name: String,
+    pub selected: bool,
+}
+
+#[styled_component(FileName)]
+fn from(FileNameProps { name, selected }: &FileNameProps) -> Self {
+    html! {
+        <div data-selected={if *selected {"true"} else {"false"}} class={css!(r#"
+                display: flex;
+                align-items: center;
+                padding: 0.5rem;
+                cursor: pointer;
+                transition: background-color 0.2s ease-in-out;
+                &:hover {
+                    background-color: rgba(0, 0, 0, 0.1);
+                }
+                &[data-selected="true"] {
+                    background-color: rgba(0, 0, 0, 0.1);
+                }
+            "#)}>
+            {name}
+        </div>
     }
 }
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,6 +2,7 @@ pub mod app;
 pub mod file_explorer;
 pub mod layout;
 pub mod menubar;
+pub mod new_file_button;
 pub mod secondary_panel;
 pub mod tab;
 pub mod tab_container;
